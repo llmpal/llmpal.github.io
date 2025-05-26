2bfca988da1c08f166adbb4008059ccc97da3605
Ankit Sarkar <ankt.srkr@gmail.com>
2025-05-26 20:52:23 +0100

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeRapide from 'starlight-theme-rapide'
+
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+  integrations: [
+      starlight({    
+          plugins: [starlightThemeRapide()],
+          title: 'PromptR',
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+          sidebar: [
+              {
+                  label: 'Prompts',
+                  autogenerate: { directory: 'prompts' },
+              },
+          ],
+      }),
 	],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,18 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
 	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+	prompts: defineCollection({
+		type: "data",
+		schema: z.object({
+			name: z.string(),
+			author: z.string(),
+			description: z.string(),	
+			version: z.string(),
+			system_prompts:z.array(z.string()),
+			inputs: z.array(z.string())
+		})
+	}),
 };
